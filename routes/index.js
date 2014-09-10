ae0d3ff97cb3058e5cab894920a4bfd9608146f9
Santiago G. Marín <santiago@finaldevstudio.com>
2014-09-10 15:00:37 -0300

    Simplified index routing logic

--- routes/index.js
+++ routes/index.js
@@ -5,25 +5,17 @@ var express = require('express');
 var router = express.Router();
 
 /**
- * This is the root URI.
- */
-router.get('/', function (req, res) {
-
-    res.render('index', {
-        title: "fi-mean"
-    });
-
-});
-
-/**
  * Provide any route that doesn't match /api/... or /templates/... with the default view.
  * 
- * You must configure the rest of the public routes in Angular.
+ * You must configure the rest of the public routes in Angular using /api/ and /templates/.
  */
-router.get(/^\/(?:(?!api|templates)[^\/])+\/?.*/gi, function (req, res) {
+router.get([
+    '/',
+    /^\/(?:(?!api|templates)[^\/])+\/?.*/gi
+], function (req, res) {
 
     res.render('index', {
-        title: "fi-mean"
+        title: "Telemedicina"
     });
 
 });
