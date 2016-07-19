9a3f310b761855864806aefa392aacfb9ddd2dfc
Santiago G. Marín <santiago@finaldevstudio.com>
2016-07-19 15:46:14 -0400

    Updated server components

--- server/components/errors.js
+++ server/components/errors.js
@@ -1,11 +1,14 @@
 'use strict';
 
+const CONSTS = component('consts');
+
+const HTTP_CODE_NOTFOUND = CONSTS.CODES.HTTP.NOTFOUND;
+const HTTP_CODE_ERR = CONSTS.CODES.HTTP.ERR;
+
 const ASSETS_OR_API_REGEXP = /^\/(assets|api)\//i;
 const ERR_NOT_FOUND = 'ERR.RESOURCE-NOT-FOUND';
 const NOT_FOUND_REDIRECT = '/lost?url=';
 const ERR_REDIRECT = '/error';
-const NOT_FOUND_CODE = 404;
-const ERR_CODE = 500;
 const NL = '\n';
 
 const DIR_OPTS = {
@@ -21,7 +24,7 @@ module.exports = (app) => {
   app.use((req, res, next) => {
     var err = new Error(ERR_NOT_FOUND);
 
-    err.status = NOT_FOUND_CODE;
+    err.status = HTTP_CODE_NOTFOUND;
 
     next(err);
   });
@@ -30,7 +33,7 @@ module.exports = (app) => {
    * Error handler.
    */
   app.use((err, req, res, next) => { // eslint-disable-line
-    res.status(err.status || ERR_CODE);
+    res.status(err.status || HTTP_CODE_ERR);
 
     /* Log the error */
     console.log(NL);
@@ -45,7 +48,7 @@ module.exports = (app) => {
     }
 
     /* If it's a 404 render the lost page */
-    if (err.status === NOT_FOUND_CODE) {
+    if (err.status === HTTP_CODE_NOTFOUND) {
       return res.redirect(NOT_FOUND_REDIRECT + encodeURIComponent(req.originalUrl));
     }
 
