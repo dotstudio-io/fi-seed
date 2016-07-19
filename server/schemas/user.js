8802de1630b3de4d59e22c463febc1e006860385
Santiago G. Marín <santiago@finaldevstudio.com>
2016-07-19 14:25:38 -0400

    Updated schemas

--- server/schemas/user.js
+++ server/schemas/user.js
@@ -1,7 +1,24 @@
 'use strict';
 
-var validator = require('validator');
-var bcrypt = require('bcrypt');
+const validator = require('validator');
+const bcrypt = require('bcrypt');
+
+const CONSTS = component('consts');
+
+/* Genders */
+const GENDER_FEMALE = CONSTS.GENDERS.FEMALE;
+const GENDER_MALE = CONSTS.GENDERS.MALE;
+
+/* Roles */
+const ROLE_ADMIN = CONSTS.ROLES.ADMIN;
+const ROLE_USER = CONSTS.ROLES.USER;
+
+/* Password hashing rounds */
+const ROUNDS = 8;
+
+const PASSWORD = 'password';
+const EMAIL = 'email';
+const USER = 'user';
 
 module.exports = (Schema) => {
 
@@ -17,10 +34,7 @@ module.exports = (Schema) => {
       required: true,
       unique: true,
       index: true,
-
-      validate: function (val) {
-        return validator.isEmail(val);
-      }
+      validate: validator.isEmail
     },
 
     password: {
@@ -29,13 +43,17 @@ module.exports = (Schema) => {
     },
 
     gender: {
-      type: Schema.Types.ObjectId,
-      ref: 'static.gender'
+      type: String,
+      enum: [
+        GENDER_FEMALE, GENDER_MALE
+      ]
     },
 
     role: {
-      type: Schema.Types.ObjectId,
-      ref: 'static.role'
+      type: String,
+      enum: [
+        ROLE_ADMIN, ROLE_USER
+      ]
     }
 
   }, {
@@ -51,8 +69,8 @@ module.exports = (Schema) => {
     var user = this;
 
     /* Hash password if changed */
-    if (user.isModified('password')) {
-      return bcrypt.hash(user.password, 8, (err, hash) => {
+    if (user.isModified(PASSWORD)) {
+      return bcrypt.hash(user.password, ROUNDS, (err, hash) => {
         if (err) {
           return next(err);
         }
@@ -67,22 +85,11 @@ module.exports = (Schema) => {
   });
 
   /**
-   * Find a user by it's email.
+   * Find a user by it's email (Promised).
    */
-  schema.static('findByEmail', function (email, done) {
-    var query = this.model('user').findOne();
-
-    query.where('email').equals(email);
-
-    if (!done) {
-      return query;
-    }
-
-    if (typeof done === 'function') {
-      return query.exec(done);
-    }
-
-    throw new TypeError('Callback must be empty or a function!');
+  schema.static('findByEmail', function (email) {
+    return this.model(USER).findOne()
+      .where(EMAIL).equals(email);
   });
 
   return schema;
